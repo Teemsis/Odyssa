31cb7207456e74defc7432f81a502011b60b0d70
Teemsis <teemsis@live.fr>
2014-05-26 11:52:22 +0200

    commit

--- Data/Data.py
+++ Data/Data.py
@@ -1,1 +1,5 @@
-#!/usr/bin/python
+class Data:
+    def __init__(self):
+        raise Exception("Abstract class 'Data' can't be instancied")
+    def plot(self):
+        raise Exception("This method must be implemented")
--- Learner/Learner.py
+++ Learner/Learner.py
@@ -1,1 +1,5 @@
-#!/usr/bin/python
+class Learner:
+    def __init__(self):
+        raise Exception("Abstract class 'Learner' can't be instancied")
+    def run(self):
+        raise Exception("This method must be implemented")
